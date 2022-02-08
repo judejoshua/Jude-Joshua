1bdcad69ec870da7c07519c18a969d852edbc8ee
Jude Joshua <officialuby@gmail.com>
2022-02-08 10:22:18 +0100

    Landing 1.0

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    $title = 'Development Ongoing';
+    include './components/header.php';
+?>
+    </head>
+
+    <body>
+        <div class="preloader">
+            <h1>JUDE JOSHUA</h1>
+        </div>
+        <div class="wrapper">
+            <div class="landing">
+                <div class="logo">
+                    <img alt="" logo src="/assets/img/logo-variant.png">
+                </div>
+                <div class="title">
+                    <h1>
+                        <span class="text-style-1">You </span>landed here,<br/>while I am still<br/>working.
+                    </h1>
+                </div>
+                <img id="arrow" src="/assets/img/Arrow 1.png" srcset="/assets/img/Arrow 1@2x.png 2x, /assets/img/Arrow 1@3x.png 3x">
+                <div class="subtitle">
+                    <p>
+                        If you'd like to collab on a project, you can send me an
+                        <a href="mailto:hello@judejoshua.me">email<i class="mdi mdi-arrow-right"></i></a> or you can take a look at my
+                        <a href="">résumé<i class="mdi mdi-arrow-right"></i></a>.
+                    </p>
+                </div>
+            </div>
+            <div class="bg">
+                <div id="vector-1"><img alt="bg-vector" src="/assets/img/vector.png"></div>
+                <div id="vector-2"><img alt="bg-vector" src="/assets/img/Vector(2).png"></div>
+                <div id="vector-3"><img alt="bg-vector" src="/assets/img/Vector(2).png"></div>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-top">
+                <a href="mailto:hello@judejoshua.me" target="_blank" class="Email">Email</a>
+                <a href="" target="_blank" class="Behance">Behance</a>
+                <a href="" target="_blank" class="LinkedIn">LinkedIn</a>
+                <a href="" target="_blank" class="Twitter">Twitter</a>
+            </div>
+            <span class="copyright">
+                (c) Jude Joshua, 2022
+              </span>
+        </footer>
+        <span class="cursor"></span>
+    </body>
+
+    </html>
