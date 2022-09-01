37c54fa7d76c6e7527a28c1b8d1e2f1fd3d5acea
Jude Joshua <officialuby@gmail.com>
2022-09-01 12:27:58 +0100

    updated git-ignore file to show jameplus-consult

--- app/views/portfolio/case_study/index.php
+++ app/views/portfolio/case_study/index.php
@@ -88,7 +88,7 @@
                                                 }
                                             echo
                                             '</p>
-                                        </div>                                    
+                                        </div>                  
                                     ';
                                 }else{
                                     echo'
